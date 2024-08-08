878d684c98d7d2b01b7e0e72caf31ff92fcc28c4
Andrew Hunt <andrew.hunt@archonsystems.com>
2024-08-08 13:59:45 -0400

    Update web sample with LE staging support (including additional issuers)

--- samples/Web/Startup.cs
+++ samples/Web/Startup.cs
@@ -1,13 +1,24 @@
 ﻿// Copyright (c) Nate McMaster.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Text;
+
 namespace Web;
 
 public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddLettuceEncrypt(_ => { }, false);
+        services.AddLettuceEncrypt(o =>
+        {
+            if (o.UseStagingServer)
+            {
+                o.AdditionalIssuers = Directory.GetFiles("Testing", "*.pem")
+                    .Select(File.ReadAllBytes)
+                    .Select(pem => Encoding.UTF8.GetString(pem))
+                    .ToArray();
+            }
+        });
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
