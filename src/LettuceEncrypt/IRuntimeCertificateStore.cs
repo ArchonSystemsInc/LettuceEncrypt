f923450a172fdd884ee439e1e2f699f3461bc156
Andrew Hunt <andrew.hunt@archonsystems.com>
2024-08-08 14:22:29 -0400

    Adjust file headers

--- src/LettuceEncrypt/IRuntimeCertificateStore.cs
+++ src/LettuceEncrypt/IRuntimeCertificateStore.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Nate McMaster.
+﻿// Copyright (c) Nate McMaster & Archon Systems Inc.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Security.Cryptography.X509Certificates;
@@ -11,14 +11,14 @@ namespace LettuceEncrypt
     public interface IRuntimeCertificateStore
     {
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="domainName"></param>
         /// <param name="certificate"></param>
         /// <returns></returns>
         Task<X509Certificate2> AddCertWithDomainNameAsync(string domainName, X509Certificate2 certificate);
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="domainName"></param>
         /// <param name="certificate"></param>
@@ -26,43 +26,43 @@ namespace LettuceEncrypt
         Task<X509Certificate2> AddChallengeCertWithDomainNameAsync(string domainName, X509Certificate2 certificate);
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="domainName"></param>
         /// <returns></returns>
         Task<X509Certificate2?> GetCertAsync(string domainName);
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="domainName"></param>
         /// <returns></returns>
         Task<X509Certificate2?> GetChallengeCertAsync(string domainName);
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <returns></returns>
         Task<bool> AnyChallengeCertAsync();
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="domainName"></param>
         /// <returns></returns>
         Task<bool> ContainsCertForDomainAsync(string domainName);
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <returns></returns>
         Task<IEnumerable<string>> GetAllCertDomainsAsync();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="domainName"></param>
         /// <returns></returns>
         Task<bool> RemoveCertAsync(string domainName);
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="domainName"></param>
         /// <returns></returns>
